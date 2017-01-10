8bc09928d1a6c261daf4fba4918a78a93db79f9f
med amine <amineammach@hotmail.com>
2017-01-10 04:21:50 -0600

    another commit

--- src/main/java/wasdev/sample/servlet/SimpleServlet.java
+++ src/main/java/wasdev/sample/servlet/SimpleServlet.java
@@ -20,8 +20,9 @@ public class SimpleServlet extends HttpServlet {
      */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    
         response.setContentType("text/html");
-        response.getWriter().print("hello irisi ");
+        response.getWriter().print("hello world ");
     }
 
 }
